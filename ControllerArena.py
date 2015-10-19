f95d3227492db009a1d1adcb1eecd5c9a2d4f6ce
vdorbs <victor.d.dorobantu@gmail.com>
2015-10-19 15:44:59 -0400

    Added visual logger, refactored ControllerArena as class

--- ControllerArena.py
+++ ControllerArena.py
@@ -1,55 +1,48 @@
 import socket
 import time
-import UnicycleKinematic as UK
-import ProportionalController as PC
-import numpy as np
-from math import pi
 
-# Socket address
-HOST = '127.0.0.1'
-PORT = 8080
-# Open socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect to socket
-s.connect((HOST, PORT))
+class ControllerArena:
+    def __init__(self):
+        # Socket address
+        HOST = '127.0.0.1'
+        PORT = 8080
+        # Open socket
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to socket
+        self.s.connect((HOST, PORT))
 
-# Time step
-DT = 0.05
-# Reference vector
-ref = np.array([[10], [0], [0]])
-# Initial controller state
-x0_c = 0
-# Proportional gain
-kp = 2
-# Initialize controller
-pc = PC.ProportionalController(x0_c, kp)
-# Initial plant state
-x0_p = np.array([[0], [0], [0]])
-# Initialize plant
-uk = UK.UnicycleKinematic(x0_p)
-# Initial plant output (controller input delayed 1 step to break algebraic loop)
-y = np.array([[0], [0], [0]])
+    def sim(self, Controller, kp, Plant, ref, x0_c, x0_p, y0, dt, t_stop):
+        # Initialize controller
+        C = Controller(x0_c, kp)
+        # Initialize plant
+        P = Plant(x0_p)
+        # Initialize output
+        y = y0
+        # Initialize time
+        t = 0
+        # Simulation
+        while t < t_stop:
+            # Get controller output
+            u = C.getOutput(ref, y)
+            # Update controller state
+            C.updateState(ref, y, dt)
+            # Get plant output
+            y = P.getOutput(u)
+            # Update controller output
+            P.updateState(u, dt)
+            # Log plant output
+            self.s.sendall(str(y))
+            # Update time
+            t += dt
+            # Delay time step
+            time.sleep(dt)
+        # Get final controller output
+        u = C.getOutput(ref, y)
+        # Get final plant output
+        y = P.getOutput(u)
+        # Log plant output
+        self.s.sendall(str(y))
 
-# Simulation
-while abs(ref[0]-y[0] > 1e-6):
-    # Controller current output
-    u = pc.getOutput(ref, y)
-    # Step controller forward
-    pc.updateState(ref, y, DT)
-    # Plant current output
-    y = uk.getOutput(u)
-    # Step plant forward
-    uk.updateState(u, DT)
-    # Log plant output
-    s.sendall(str(y))
-    # Delay by time step
-    time.sleep(DT)
-# Controller after final step
-u = pc.getOutput(ref, y)
-# Plant after final step
-y = uk.getOutput(u)
-# Log final plant output
-s.sendall(str(y))
-
-# Close connection
-s.close()
+    def __del__(self):
+        # Close connection
+        self.s.close()
