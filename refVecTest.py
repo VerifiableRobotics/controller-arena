ead5fb54e37ac5d7cb51e8dfef1b0a47666ce9c8
vdorbs <victor.d.dorobantu@gmail.com>
2015-11-02 15:25:03 -0500

    Changed test script name to avoid collision

---
+++ refVecTest.py
@@ -0,0 +1,30 @@
+from ControllerArena import *
+# import ProportionalController as PC
+import PController as PC
+import refVec as rv
+import UnicycleKinematic as UK
+import numpy as np
+import json
+
+# Time step
+dt = 0.05
+# Stop time
+t_stop = 10
+# Reference vector
+ref = np.array([[10], [7], [2]])
+# Initial controller state
+x0_c = 0
+# Proportional gain
+kp = 9
+# Initial plant state
+x0_p = np.array([[0], [0], [0]])
+# Initial plant output (controller input delayed 1 step to break algebraic loop)
+y0 = np.array([[0], [0], [0]])
+
+configs = json.dumps({'config': [{'x': 1, 'y': 2, 'xlabel': 'x (m)', 'ylabel': 'y (m)'}, {'x': 0, 'y': 3, 'xlabel': 't (s)', 'ylabel': 'theta (rad)'}]})
+
+ca = ControllerArena()
+
+ca.config(configs)
+# ca.sim(PC.ProportionalController, kp, UK.UnicycleKinematic, ref, x0_c, x0_p, y0, dt, t_stop)
+ca.sim(PC.PController, kp, UK.UnicycleKinematic, ref, x0_c, x0_p, y0, dt, t_stop)
